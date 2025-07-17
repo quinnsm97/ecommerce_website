ec2c85eb51c94d97be866907333c73d6090d7e27
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-07-17 21:06:09 +1000

    added categories schema and created DELETE CREATE and GET routes

--- app.py
+++ app.py
@@ -34,6 +34,19 @@ class Product(db.Model):
         self.price = price
         self.stock = stock
 
+        abc@abc
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    #Attributes
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(255)) 
+
+    # Not necessary, as SQLAlchemy automatically initialises it for Marshmallow Schemas
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
 # Create a class for ProductSchema
 class ProductSchema(SQLAlchemyAutoSchema):
@@ -41,11 +54,23 @@ class ProductSchema(SQLAlchemyAutoSchema):
         model = Product
         load_instance = True
 
+# Create a class for CategorySchema
+class CategorySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+        load_instance = True
+
 # ProductSchema instance to handle multiple products
 products_schema = ProductSchema(many=True)
 
 # ProductSchema instance to handle single product
 product_schema = ProductSchema()
+
+# CategorySchema instance to handle multiple Categories
+categories_schema = CategorySchema(many=True)
+
+# CategorySchema instance to handle single Category
+category_schema = CategorySchema()
 
 @app.cli.command("create") # flask create
 def create_table():
@@ -78,6 +103,23 @@ def seed_tables():
     db.session.add(product1)
     db.session.add(product2)
     
+    # Create a list of categories object
+    categories = [
+        Category(
+            name = "Electronics",
+            description = "Gadgets and tech"
+        ), 
+        Category(
+            name = "Books",
+            description = "Fiction, non-fiction and everything in between"
+        ), 
+        Category(
+            name = "Supplies"
+        )
+    ]
+    # Add the list to the session
+    db.session.add_all(categories)
+
     db.session.commit()
 
     print("Table seeded successfully.")
@@ -189,6 +231,100 @@ def update_product(product_id):
     else:
         # acknowledgement message
         return {"message": f"Product with id '{product_id}' does not exist."}, 404
+
+# GET all /categories
+@app.route('/categories')
+def get_categories():
+    # Define the GET Statement, write the query
+    stmt = db.select(Category)
+    # Execute it - Scalar(s)
+    categories_list = db.session.scalars(stmt)
+    # Serialise it
+    data = categories_schema.dump(categories_list)
+    # return it
+    return jsonify(data)
+
+# GET a category
+# GET /categories/{id}
+@app.route("/categories/<int:category_id>")
+def get_single_category(category_id):
+    # Define the GET Statement, write the query
+    stmt = db.select(Category).where(Category.id == category_id)
+    # Execute it - Scalar(s)
+    category = db.session.scalar(stmt)
+
+    if category:
+        # Serialise it
+        data = category_schema.dump(category)
+        # return it
+        return jsonify(data) 
+    else:
+        return jsonify({"message": f"Category with id {category_id} doesn't exist"}), 404
+
+# CREATE a category
+# POST /categories
+@app.route("/categories", methods=["POST"])
+def create_category():
+    # Statement: INSERT INTO categories(arg1, arg2, ..) VALUES (value1, value2, ..)
+    # Get the body JSON data
+    body_data = request.get_json()
+    # Create a Category object and pass on the values
+
+    new_category = Category(
+        name = body_data.get("name"),
+        description = body_data.get("description")
+    )
+    # Add to the session and commit
+    db.session.add(new_category)
+    db.session.commit()
+
+    # Return the newly created product
+    data = product_schema.dump(new_category)
+    return jsonify(data), 201
+
+# DELETE a category
+# DELETE /categories/id
+@app.route("/categories/<int:category_id>", methods=["DELETE"])
+def delete_category(category_id):
+    # Statement: DELETE * FROM categories WHERE id=category_id;
+    # Find the product with the category_id from the database
+    # Statement: SELECT * FROM categories WHERE id = category_id;
+    # Method 1:
+    # stmt = db.select(Category).where(Category.id == category_id)
+    stmt = db.select(Category).filter_by(id=category_id)
+    category = db.session.scalar(stmt)
+    # Method 2:
+    # category = Category.query.get(category_id)
+    # if it exists
+    if category:
+        # delete the category
+        db.session.delete(category)
+        db.session.commit()
+        # send an acknowledgement message
+        return {"message": f"Category with id {category_id} deleted successfully."}
+    # else
+    else:
+        # send an acknowledgement message
+        return {"message": f"Category with id {category_id} does not exist."}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
